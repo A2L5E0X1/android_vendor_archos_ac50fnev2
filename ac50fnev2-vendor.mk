09e302d6aac8fc80d04b9cf4c8d67b36d3e69525
A2L5E0X1 <alexed25@protonmail.com>
2021-09-15 19:54:19 +0200

    ac50fnev2: pin libstagefright with MtkColorFormat support

--- ac50fnev2-vendor.mk
+++ ac50fnev2-vendor.mk
@@ -23,6 +23,7 @@ PRODUCT_COPY_FILES += \
     vendor/archos/ac50fnev2/proprietary/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     vendor/archos/ac50fnev2/proprietary/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     vendor/archos/ac50fnev2/proprietary/lib/libfs_mgr.so:system/lib/libfs_mgr.so \
+    vendor/archos/ac50fnev2/proprietary/lib/libstagefright.so:system/lib/libstagefright.so \
     vendor/archos/ac50fnev2/proprietary/vendor/bin/akmd09911:system/vendor/bin/akmd09911 \
     vendor/archos/ac50fnev2/proprietary/vendor/bin/akmd8963:system/vendor/bin/akmd8963 \
     vendor/archos/ac50fnev2/proprietary/vendor/bin/akmd8975:system/vendor/bin/akmd8975 \
